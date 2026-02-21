b517ff5bcd4d58a4ef5941c627e446fe9833feb8
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-02-21 16:13:45 -0300

    FEAT: adds GradeAverage class

---
+++ src/GradeAverage.java
@@ -0,0 +1,21 @@
+public class GradeAverage {
+    private final int firstGrade;
+    private final int secondGrade;
+
+    public GradeAverage(int firstGrade, int secondGrade) {
+        this.firstGrade = firstGrade;
+        this.secondGrade = secondGrade;
+    }
+
+    public int getFirstGrade() {
+        return firstGrade;
+    }
+
+    public int getSecondGrade() {
+        return secondGrade;
+    }
+
+    public int CalcGradeAverage(int firstGrade, int secondGrade) {
+        return firstGrade + secondGrade / 2;
+    }
+}
