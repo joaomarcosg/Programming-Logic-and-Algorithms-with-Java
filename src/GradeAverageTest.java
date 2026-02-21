f8bb8f599c1e01dca594c76f5070c66b9338dadd
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-02-21 16:08:46 -0300

    TEST: adds GradeAverage test

---
+++ src/GradeAverageTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+public class GradeAverageTest {
+
+    @Test
+    void calcGradeAverage() {
+        int firstGrade = 7;
+        int secondGrade = 8;
+
+        int result = GradeAverage.CalcGradeAverage(firstGrade, secondGrade);
+
+        assertEquals(7, result);
+    }
+
+}
