ccf6e54dd995238080613d9c30343c4f23353289
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-02-21 16:33:45 -0300

    FIX: fixes type parameter

--- src/PaymentOptions.java
+++ src/PaymentOptions.java
@@ -5,7 +5,7 @@ public class PaymentOptions {
         this.price = price;
     }
 
-    public static double CalcCashPayment(int price) {
+    public static double CalcCashPayment(double price) {
 
         return 0.10 * price;
     }
