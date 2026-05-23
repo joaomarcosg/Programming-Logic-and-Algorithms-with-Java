ea193245606c55957238446af3ac5c3406f2c253
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-05-23 20:20:49 -0300

    test add ParkingMeter excercise test

---
+++ src/Conditions/ParkingMeterTest.java
@@ -0,0 +1,13 @@
+package Conditions;
+
+import org.junit.jupiter.api.Test;
+
+public class ParkingMeterTest {
+    @Test
+    void lenghtOfStay() {
+        double firstValue = 1;
+        double secondValue = 1.75;
+        double thirdValue = 3;
+
+    }
+}
