c4ee82ed70f1da47b8b965837226f7e5e8cfee37
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-05-25 12:49:23 -0300

    test: adds NormalizeInput exercise test

---
+++ src/test/java/exercises/NormalizeInputTest.java
@@ -0,0 +1,20 @@
+package test.java.exercises;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NormalizeInputTest {
+    @Test
+    @DisplayName("Should remove spaces at the border, reduce internal spaces and convert to lowercase")
+    void shouldNormalizeTextWithMultipleSpacesAndUppercase() {
+
+        String input = "  joão   Silva";
+        NormalizeInput normalizer = new NormalizerInput(input);
+        String expected = "joão silva";
+
+        String get = normalizer.normalize();
+
+        assertEquals(expected, get, "The text has not been normalized")
+
+    }
+}
