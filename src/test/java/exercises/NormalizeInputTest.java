cf2c076159cab37ed7d89581f00ec379443b7ff4
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-05-25 13:20:40 -0300

    test: adds more edge cases

--- src/test/java/exercises/NormalizeInputTest.java
+++ src/test/java/exercises/NormalizeInputTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class NormalizeInputTest {
     @Test
@@ -19,5 +20,15 @@ public class NormalizeInputTest {
 
         assertEquals(expected, get, "The text has not been normalized");
 
+    }
+
+    @Test
+    @DisplayName("Should return null when input is null")
+    void shouldReturnNullWhenInputIsNull() {
+        NormalizeInput normalizer = new NormalizeInput(null);
+
+        String expected = normalizer.normalize();
+
+        assertNull(expected, "The method should return null when input is null");
     }
 }
