5366400502338ef82332590d292a193b999f922f
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-05-25 12:59:35 -0300

    test: fixes test

--- src/test/java/exercises/NormalizeInputTest.java
+++ src/test/java/exercises/NormalizeInputTest.java
@@ -1,7 +1,10 @@
 package test.java.exercises;
 
+import main.java.exercises.NormalizeInput;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NormalizeInputTest {
     @Test
@@ -9,12 +12,12 @@ public class NormalizeInputTest {
     void shouldNormalizeTextWithMultipleSpacesAndUppercase() {
 
         String input = "  joão   Silva";
-        NormalizeInput normalizer = new NormalizerInput(input);
+        NormalizeInput normalizer = new NormalizeInput(input);
         String expected = "joão silva";
 
         String get = normalizer.normalize();
 
-        assertEquals(expected, get, "The text has not been normalized")
+        assertEquals(expected, get, "The text has not been normalized");
 
     }
 }
