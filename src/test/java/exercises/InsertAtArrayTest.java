f6dbbfef9c0ab38e6aa67ebc92cc71337b113f99
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-05-30 19:19:41 -0300

    test: fixes InsertAtArray test changing the type to List instead of int

--- src/test/java/exercises/InsertAtArrayTest.java
+++ src/test/java/exercises/InsertAtArrayTest.java
@@ -3,19 +3,23 @@ package test.java.exercises;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InsertAtArrayTest {
     @Test
     @DisplayName("Should insert at beginning of the array")
     void shouldInsertAtBeginningOfArray() {
-        int[] arr = new int[]{2, 3, 4};
+        List<Integer> arr = new ArrayList<>(Arrays.asList(2, 3, 4));
         int value = 1;
 
-        InsertAtArray insertAtArray = new InsertAtArray(arr, value);
-        int[] expected = new int[]{1, 2, 3, 4};
+        InsertAtArray insertAtArray = new InsertAtArray();
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
 
-        int[] get = insertAtArray.insertAtBeginning();
+        List<Integer> get = insertAtArray.insertAtBeginning(arr, value);
 
         assertEquals(expected, get);
 
