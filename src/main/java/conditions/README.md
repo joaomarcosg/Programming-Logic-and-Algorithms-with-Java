be55ff629cfbe628d1d92417dd9e0d25baa5d000
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-07-04 18:00:02 -0300

    test: adds ParkingMeter test

---
+++ src/main/java/conditions/README.md
@@ -0,0 +1,7 @@
+# List of conditional exercises in Java
+
+### Exercise 1 - Even or Odd 
+
+*Write a program that reads a number and reports whether it is odd or even.*
+
+**solution:** [EvenOrOddNumber](src/main/java/condition/EvenOrOddNumber)
--- src/test/java/conditions/ParkingMeterTest.java
+++ src/test/java/conditions/ParkingMeterTest.java
@@ -1,13 +1,23 @@
 package test.java.conditions;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ParkingMeterTest {
-    @Test
-    void lenghtOfStay() {
-        double firstValue = 1;
-        double secondValue = 1.75;
-        double thirdValue = 3;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class ParkingMeterTest {
+    private ParkingMeter parkingMeter;
+
+    @BeforeEach
+    void setUp() {
+        parkingMeter = new ParkingMeter();
+    }
+
+    @Test
+    void shouldReturnTimeWithoutChange_WhenAmountIsExact() {
+        ResultParkingMeter result = parkingMeter.lenghtOfStay(1.75);
+
+        assertEquals("60 min", result.time);
+        assertEquals(0.0, result.change);
     }
 }
