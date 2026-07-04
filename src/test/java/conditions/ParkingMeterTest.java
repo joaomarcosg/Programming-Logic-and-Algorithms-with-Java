2a1e41b5b78b5bf83ef1ade3dfe3e5df8552c983
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-07-04 18:42:24 -0300

    test: adds more edge cases

--- src/test/java/conditions/ParkingMeterTest.java
+++ src/test/java/conditions/ParkingMeterTest.java
@@ -22,4 +22,28 @@ public class ParkingMeterTest {
         assertEquals("60 min", result.time());
         assertEquals(0.0, result.change());
     }
+
+    @Test
+    void shouldCalculateChangeCorrectly_WhenAmountIsGreater() {
+        ResultParkingMeter result = parkingMeter.lengthOfStay(5.00);
+
+        assertEquals("120 min", result.time());
+        assertEquals(2.00, result.change());
+    }
+
+    @Test
+    void shouldCalculateChangeCorrectly_WhenAmountIsRandom() {
+        ResultParkingMeter result = parkingMeter.lengthOfStay(2.00);
+
+        assertEquals("60 min", result.time());
+        assertEquals(0.25, result.change());
+    }
+
+    @Test
+    void shouldReturnInsufficientAmount_WhenAmountIsLess() {
+        ResultParkingMeter result = parkingMeter.lengthOfStay(0.75);
+
+        assertEquals("Insufficient amount", result.time());
+        assertEquals(0.00, result.change());
+    }
 }
